292fc8a3cfaf1a517e8f550e238a5a82babc8761
WarunaSLFI <warunaslfi@gmail.com>
2026-03-22 23:26:50 +0200

    Stage 2b: Add formatted print output

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main{
     public static void main(String[] args){
+        
         String studentName = "Waruna";
         int studentAge = 39;
         double grade = 8.5;
         boolean isEnrolled = true;
 
-        System.out.println(studentName);
-        System.out.println(studentAge);
-        System.out.println(grade);
-        System.out.println(isEnrolled);
+        System.out.println("Student Name : " + studentName);
+        System.out.println("Student Age : " + studentAge);
+        System.out.println("Grade : " + grade);
+        System.out.println("Enrolled : " + isEnrolled);
     }
 }
